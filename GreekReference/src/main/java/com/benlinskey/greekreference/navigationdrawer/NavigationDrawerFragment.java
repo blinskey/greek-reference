30defc3721899b2f7abe0df9be3b495c7f451250
Benjamin Linskey <git@linskey.org>
2024-07-09 11:57:58 -0400

    Optimize imports

--- GreekReference/src/main/java/com/benlinskey/greekreference/navigationdrawer/NavigationDrawerFragment.java
+++ GreekReference/src/main/java/com/benlinskey/greekreference/navigationdrawer/NavigationDrawerFragment.java
@@ -16,28 +16,25 @@
 
 package com.benlinskey.greekreference.navigationdrawer;
 
-import android.support.v4.view.GravityCompat;
-import android.support.v7.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Gravity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import com.benlinskey.greekreference.R;
