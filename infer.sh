ae4448535d67abdc8b0fba9b8312680eaf70a51a
Benjamin Linskey <git@linskey.org>
2025-10-23 14:50:54 -0400

    Remove obsolete and unused files

--- infer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Performs a clean build and runs Infer (see fbinfer.com).
-# The "infer" binary must be in $PATH.
-
-rm -rf infer-out
-./gradlew clean
-infer -- ./gradlew assembleDebug
-
